b24881cca3a29e85eba79f4fd4b545f7e739d4f9
Matthijs <matthijs@MBP-Matthijs.local>
2017-07-18 15:40:39 +0200

    init

---
+++ AmazingViews/FantasticView.swift
@@ -0,0 +1,9 @@
+//
+//  FantasticView.swift
+//  AmazingViews
+//
+//  Created by Matthijs on 18/07/2017.
+//  Copyright © 2017 mvdlinden. All rights reserved.
+//
+
+import Foundation
---
+++ AmazingViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmazingViews
+//
+//  Created by Matthijs on 18/07/2017.
+//  Copyright © 2017 mvdlinden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
